ac5f19cbd78fc70e194239f07748305f1c76c2a9
v1605 <55302877+v1605@users.noreply.github.com>
2025-01-15 16:23:35 -0500

    Quick fixes
    Audio now supports formated cards to be mapped in the ui (support for different readers having different behavior) and vite now will properly proxy file requests (just remove the ws:// from the env file so its only the ip)

--- svelte-esp32/vite.config.ts
+++ svelte-esp32/vite.config.ts
@@ -2,10 +2,21 @@ import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { viteSingleFile } from "vite-plugin-singlefile"
 
+function getBackendWs(){
+  const backendIP = "ws://" + getBackendIP();
+  console.log('Configured WebSocket proxy target:', backendIP);
+  return backendIP;
+}
+
+function getBackendHttp(){
+  const backendIP = "http://" + getBackendIP();
+  console.log('Configured Http proxy target:', backendIP);
+  return backendIP;
+}
+
 function getBackendIP() {
   const env = loadEnv("all", process.cwd());
-  const backendIP = env.VITE_ESP_IP || 'ws://localhost:8080';
-  console.log('Configured WebSocket proxy target:', backendIP);
+  const backendIP = env.VITE_ESP_IP || 'localhost:8080';
   return backendIP;
 }
 
@@ -14,7 +25,15 @@ export default defineConfig({
   server: {
     proxy: {
       '/ws': {
-        target: getBackendIP(), // Backend server URL
+        target: getBackendWs(), // Backend server URL
+        changeOrigin: true, // Adjust the `Host` header to match the target,
+      },
+      '/file': {
+        target: getBackendHttp(), // Backend server URL
+        changeOrigin: true, // Adjust the `Host` header to match the target,
+      },
+      '/get-folder-contents': {
+        target: getBackendHttp(), // Backend server URL
         changeOrigin: true, // Adjust the `Host` header to match the target,
       },
     },
